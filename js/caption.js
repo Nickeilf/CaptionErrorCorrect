2bca76fc63b3b9d40b7ac827cb896e6a3998e96e
marioquer <marioquer@hotmail.com>
2017-02-19 16:16:31 +0800

    new

--- js/caption.js
+++ js/caption.js
@@ -1,3 +1,12 @@
+/*
+全局变量
+*/
+window.URL = window.URL || window.webkitURL;
+
+var videoFileURL;
+var captionFileURL;
+var logFileURL;
+
 function selectFile(key){
     var text = key+"Text";
     var choser = key+"Source";
@@ -6,15 +15,32 @@ function selectFile(key){
 }
 
 function submitFile(){
-    var video = document.getElementById("videoText").value;
-    var caption = document.getElementById("captionText").value;
-    var log = document.getElementById("logText").value;
+    videoFileURL = window.URL.createObjectURL(document.getElementById('videoSource').files[0]);
+    captionFileURL = window.URL.createObjectURL(document.getElementById('captionSource').files[0]);
+    logFileURL = window.URL.createObjectURL(document.getElementById('logSource').files[0]);
     
-    
-    if(log==""||caption==""||log==""){
+    if(videoFileURL==""||captionFileURL==""||logFileURL==""){
         alert("未选择完成");
     }else{
         $("#selector").fadeOut(300);
+        var videoBlock = document.getElementById("targetVideo");
+        videoBlock.src=videoFileURL;
+        videoBlock.onload = function(){
+            window.URL.revokeObjectURL(this.src);
+        };
+        var imgBlock = document.getElementById("caption");
+        imgBlock.src=captionFileURL;
+        imgBlock.onload = function(){
+            window.URL.revokeObjectURL(this.src);
+        }
         setTimeout(function(){$("#checker").fadeIn(300)},300);
     }
+}
+
+function reset() {
+    $("#checker").fadeIn(300);
+    setTimeout(function(){$("#selector").fadeOut(300)},300);
+    /*
+        重设全局变量
+    */
 }
